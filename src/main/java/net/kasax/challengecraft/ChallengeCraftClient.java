d8b2b0d8157a2afc33c10391c754993d8ef8d02f
kasax007 <lawicrafterlp@gmail.com>
2025-04-21 12:32:22 -0400

    Initial commit

---
+++ src/main/java/net/kasax/challengecraft/ChallengeCraftClient.java
@@ -0,0 +1,10 @@
+package net.kasax.challengecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChallengeCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kasax/challengecraft/ChallengeCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kasax.challengecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChallengeCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
